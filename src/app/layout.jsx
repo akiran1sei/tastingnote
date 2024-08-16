c450df1e91fe4b8728ef7d7130a837ae869178a3
akiran1sei <nakamori.1224@gmail.com>
2024-08-16 16:30:52 +0900

    🐛 修正

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,7 @@
 import "./styles/globals.css";
 import styles from "@/app/styles/Contents.module.css";
 import { Inter } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next";
-import { AuthProvider } from "./context/AuthContext";
+
 import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import Error from "./error";
 
@@ -17,18 +16,12 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ja">
       <body className={inter.className}>
-        {/* <AuthProvider> */}
-
         <div className={styles.container}>
           <main className={styles.main}>
             <ErrorBoundary fallback={<Error />}>{children} </ErrorBoundary>
           </main>
-          <footer className={styles.footer}>
-            <SpeedInsights />
-          </footer>
+          <footer className={styles.footer}></footer>
         </div>
-
-        {/* </AuthProvider> */}
       </body>
     </html>
   );
