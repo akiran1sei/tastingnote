f0b4c3b64378516240917c9082191a1d8f03f90f
akiran1sei <nakamori.1224@gmail.com>
2024-10-12 11:19:45 +0900

    自動ログアウト時のキャッシュ削除コード追加

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -1,47 +1,50 @@
 import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
-// import { cookies } from "next/headers";
 
 export async function middleware(request) {
   const token = request.cookies.get("token")?.value;
 
   if (!token) {
-    return redirectToLogout(request);
+    return completeLogout(request);
   }
 
   try {
     const secretKey = new TextEncoder().encode(process.env.JWT_SECRET);
     const { payload } = await jwtVerify(token, secretKey);
 
-    // トークンの有効期限をチェック
     const currentTimestamp = Math.floor(Date.now() / 1000);
 
-    // トークンの有効期限が切れている場合
     if (payload.exp && payload.exp < currentTimestamp) {
       console.log("Token has expired");
-
-      // トークンの削除をここで行う
-      const response = NextResponse.redirect(
-        new URL("/pages/auth/signout", request.url)
-      );
-      response.cookies.delete("token");
-      return response;
+      return completeLogout(request);
     }
 
-    // トークンが有効な場合、リクエストを続行
     return NextResponse.next();
   } catch (error) {
-    // トークンの検証に失敗した場合
     console.error("Token verification failed:", error);
-    return redirectToLogout(request);
+    return completeLogout(request);
   }
 }
 
-function redirectToLogout(request) {
-  return NextResponse.redirect(new URL("/pages/auth/signout", request.url));
+function completeLogout(request) {
+  const response = NextResponse.redirect(
+    new URL("/pages/auth/signout", request.url)
+  );
+
+  // トークンの削除
+  response.cookies.delete("token");
+
+  // キャッシュの削除
+  response.headers.set(
+    "Cache-Control",
+    "no-store, no-cache, must-revalidate, proxy-revalidate"
+  );
+  response.headers.set("Pragma", "no-cache");
+  response.headers.set("Expires", "0");
+
+  return response;
 }
 
-// このミドルウェアを適用するパスを指定
 export const config = {
   matcher: [
     `/pages/select:path*`,
