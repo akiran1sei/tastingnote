b405062d2f7f6fca238971847203738ff46d3027
akiran1sei <nakamori.1224@gmail.com>
2024-10-27 21:21:35 +0900

    自宅にて、修正作業

--- src/app/components/molecules/Select/Select.js
+++ src/app/components/molecules/Select/Select.js
@@ -7,6 +7,7 @@ import useReadGroups from "@/app/utils/useReadGroups";
 import useSWR from "swr";
 import { useRouter } from "next/navigation";
 import { jwtDecode } from "jwt-decode";
+import { useSearchParams } from "next/router";
 import dotenv from "dotenv";
 dotenv.config();
 export function Select(context) {
@@ -16,7 +17,10 @@ export function Select(context) {
   const [isUserEmail, setIsUserEmail] = useState("");
   const [checkbox, setCheckBox] = useState([]);
   const router = useRouter();
-  console.log("user:", context.data.searchParams.user);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("user");
+  console.log("query", query);
+  // console.log("user:", context.data.searchParams.user);
   const ReadGroups = useReadGroups();
   const [selectedGroup, setSelectedGroup] = useState(""); // 選択された値を保持
   const [searchQuery, setSearchQuery] = useState(""); // 実際の検索クエリ
