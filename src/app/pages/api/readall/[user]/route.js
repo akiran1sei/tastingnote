0355f53e5aae935e51b8fd9ec0705fe354be7bc3
akiran1sei <nakamori.1224@gmail.com>
2024-10-28 16:41:58 +0900

    作業場にて、修正作業3

--- src/app/pages/api/readall/[user]/route.js
+++ src/app/pages/api/readall/[user]/route.js
@@ -7,37 +7,50 @@ export async function GET(req, res) {
     await connectDB();
     const url = new URL(req.url, `${process.env.NEXT_PUBLIC_URL}`);
     const user = url.searchParams.get("user");
-
     const userData = await UserModel.findById(res.params.user);
-    if (!user) {
-      const allItems = await BeansModel.find({
-        userEmail: userData.email,
-        groupname: { $exists: true },
-      })
-        .sort({ createdAt: 1 })
-        .limit(100)
-        .exec();
+    const allItems = await BeansModel.find({
+      userEmail: userData.email,
+      groupname: user,
+    })
+      .sort({ createdAt: 1 })
+      .limit(100)
+      .exec();
 
-      return NextResponse.json({
-        message: "読み取り成功（オール）",
-        allItems: allItems,
-        status: 200,
-      });
-    } else {
-      const allItems = await BeansModel.find({
-        userEmail: userData.email,
-        groupname: user,
-      })
-        .sort({ createdAt: 1 })
-        .limit(100)
-        .exec();
+    return NextResponse.json({
+      message: "読み取り成功（オール）",
+      allItems: allItems,
+      status: 200,
+    });
 
-      return NextResponse.json({
-        message: "読み取り成功（オール）",
-        allItems: allItems,
-        status: 200,
-      });
-    }
+    // if (!user) {
+    //   const allItems = await BeansModel.find({
+    //     userEmail: userData.email,
+    //     groupname: { $exists: true },
+    //   })
+    //     .sort({ createdAt: 1 })
+    //     .limit(100)
+    //     .exec();
+
+    //   return NextResponse.json({
+    //     message: "読み取り成功（オール）",
+    //     allItems: allItems,
+    //     status: 200,
+    //   });
+    // } else {
+    //   const allItems = await BeansModel.find({
+    //     userEmail: userData.email,
+    //     groupname: user,
+    //   })
+    //     .sort({ createdAt: 1 })
+    //     .limit(100)
+    //     .exec();
+
+    //   return NextResponse.json({
+    //     message: "読み取り成功（オール）",
+    //     allItems: allItems,
+    //     status: 200,
+    //   });
+    // }
   } catch (err) {
     return NextResponse.json({
       message: "読み取り失敗（オール）",
