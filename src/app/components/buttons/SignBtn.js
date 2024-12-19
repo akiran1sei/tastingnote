b41cc0efa4a700e8cb01e9bbc25f59906e0274e6
akiran1sei <nakamori.1224@gmail.com>
2024-12-19 12:45:53 +0900

    SNS認証システム導入のため、削除

--- src/app/components/buttons/SignBtn.js
+++
@@ -1,9 +0,0 @@
-import Buttons from "@/app/styles/Btn.module.css";
-
-export default function SignBtn() {
-  return (
-    <button type="submit" className={Buttons.sign_btn}>
-      Submit
-    </button>
-  );
-}
