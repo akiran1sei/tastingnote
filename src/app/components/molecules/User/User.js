677c5b58962bfd172e7e35281f70cad499a4bbf9
akiran1sei <nakamori.1224@gmail.com>
2025-01-24 12:45:18 +0900

    profile頁編集中

--- src/app/components/molecules/User/User.js
+++ src/app/components/molecules/User/User.js
@@ -15,21 +15,21 @@ export function ProfileComponent() {
   const [userInfo, setUserInfo] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [username, setUserName] = useState("");
-  const [isUserEmail, setIsUserEmail] = useState("");
-
+  const [userEmail, setUserEmail] = useState("");
+  const handleSubmit = async () => {};
   const handleAccountDelete = async () => {
     if (confirm("Account Delete？")) {
       if (confirm("作成したデータ全て削除しますがよろしいでしょうか？")) {
         setIsAccountDelete(true);
         try {
-          const res = await fetch(`/api/auth/delete/${isUserEmail}`, {
+          const res = await fetch(`/api/auth/delete/${userEmail}`, {
             method: "DELETE",
             headers: {
               Accept: "application/json",
               "Content-Type": "application/json",
               "Cache-Control": "no-store",
             },
-            body: JSON.stringify({ email: isUserEmail }),
+            body: JSON.stringify({ email: userEmail }),
           });
           const jsonData = await res.json();
 
@@ -51,7 +51,7 @@ export function ProfileComponent() {
     if (status === "authenticated" && session) {
       setUserInfo(session.user);
       setUserName(session.user.name);
-      setIsUserEmail(session.user.email);
+      setUserEmail(session.user.email);
       setIsLoading(false);
     } else if (status === "unauthenticated") {
       setIsLoading(false);
@@ -68,56 +68,115 @@ export function ProfileComponent() {
           <h1 className={styles.contents__title}>Profile</h1>
           {/* {session && ( */}
           <div className={styles.profile__card}>
-            <ul className={styles.profile__user__list}>
-              <li className={styles.profile__user__item}>
-                <span className={styles.profile__item__title}>username</span>
-                <span className={styles.profile__item__value}>{username}</span>
-              </li>
-              <li className={styles.profile__user__item}>
-                <span className={styles.profile__item__title}>email</span>
-                <span className={styles.profile__item__value}>
-                  {isUserEmail}
-                </span>
-              </li>
-            </ul>
-          </div>
-          {/* )} */}
-          <div className={styles.profile__btn__box}>
-            <ul className={styles.profile__btn__list}>
-              <li className={styles.profile__btn__item}>
-                <button
-                  className={`${styles.profile__btn} ${styles.profile__link__btn}`}
-                  type="button"
+            <ul
+              className={`${styles.profile__user__list} ${styles.profile__list}`}
+            >
+              <li
+                className={`${styles.profile__user__item} ${styles.profile__item}`}
+              >
+                <span
+                  className={`${styles.profile__item__title} ${styles.profile__title}`}
                 >
-                  <Link
-                    href={
-                      "https://nakamoriakira-portfolio.vercel.app/pages/contact"
-                    }
-                    passHref
-                  >
-                    お問い合わせページへ
-                  </Link>
-                </button>
+                  username
+                </span>
+                <span
+                  className={`${styles.profile__item__value} ${styles.profile__value}`}
+                >
+                  {username}
+                </span>
               </li>
               <li
-                className={`${styles.profile__btn__item} ${styles.profile__delete}`}
+                className={`${styles.profile__user__item} ${styles.profile__item}`}
               >
-                <span className={styles.caution__text}>
-                  こちらの全データ削除について、
-                  <br />
-                  アカウント削除されるとこのアカウントで作成されたデータが全て削除されます。
-                </span>
-                <button
-                  className={`${styles.profile__btn} ${styles.profile__delete__btn}`}
-                  type="button"
-                  onClick={handleAccountDelete}
-                  disabled={isAccountDelete}
+                <span
+                  className={`${styles.profile__item__title} ${styles.profile__title}`}
                 >
-                  {isLoggingOut ? "アカウント削除中.." : "全データ 削除"}
-                </button>
+                  email
+                </span>
+                <span
+                  className={`${styles.profile__item__value} ${styles.profile__value}`}
+                >
+                  {userEmail}
+                </span>
               </li>
             </ul>
+            <div className={styles.profile__setting}>
+              <div className={styles.profile__wrap}>
+                <form onSubmit={handleSubmit} className={styles.profile__form}>
+                  <div
+                    className={`${styles.profile__setting__item} ${styles.profile__item}`}
+                  >
+                    <label
+                      htmlFor="profile__name"
+                      className={`${styles.profile__setting__title} ${styles.profile__title}`}
+                    >
+                      usernameの編集
+                    </label>
+
+                    <input
+                      type="text"
+                      id="profile__name"
+                      className={styles.profile__setting__input}
+                      name="name"
+                      placeholder="username"
+                      value={username}
+                      disabled
+                    />
+                  </div>
+                  <div
+                    className={`${styles.profile__setting__item} ${styles.profile__item}`}
+                  >
+                    <label
+                      htmlFor="profile__email"
+                      className={`${styles.profile__setting__title} ${styles.profile__title}`}
+                    >
+                      emailの編集
+                    </label>
+
+                    <input
+                      type="email"
+                      id="profile__email"
+                      className={styles.profile__setting__input}
+                      name="email"
+                      placeholder="email"
+                      value={userEmail}
+                      disabled
+                    />
+                  </div>
+                </form>
+                <div className={styles.profile__btn__group}>
+                  <ul className={styles.profile__btn__list}>
+                    <li className={styles.profile__btn__item}>
+                      <button type="submit" className={styles.profile__btn}>
+                        保存
+                      </button>
+                    </li>
+
+                    <li
+                      className={`${styles.profile__btn__item} ${styles.profile__delete}`}
+                    >
+                      <span className={styles.caution__text}>
+                        ～こちらの退会について～
+                        <br />
+                        退会されますとこのアカウントで
+                        <br />
+                        作成されたデータが全て削除されます。
+                      </span>
+                      <button
+                        className={`${styles.profile__btn} ${styles.profile__delete__btn}`}
+                        type="button"
+                        onClick={handleAccountDelete}
+                        disabled={isAccountDelete}
+                      >
+                        {isLoggingOut ? "アカウント削除中.." : "退会"}
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
+          {/* )} */}
         </div>
       </>
     );
