c70598c1d7a76bfbef62f9ed829b8e76d56affc6
akiran1sei <nakamori.1224@gmail.com>
2025-02-10 16:42:18 +0900

    マニュアルページ作成中（create）

--- src/app/components/molecules/Manual/Manual.js
+++ src/app/components/molecules/Manual/Manual.js
@@ -8,7 +8,7 @@ export function ManualComponent() {
           <h1 className={manual.contents__title}>Manual</h1>
           <div className={manual.manual__select}>
             <h2 className={manual.manual__sub__title}>
-              <span className={manual.underBar}>selectページの見方</span>
+              <span className={manual.underBar}>selectの見方</span>
             </h2>
             <div className={manual.manual__img}>
               <Image
@@ -216,6 +216,308 @@ export function ManualComponent() {
                 </li>
               </ol>
             </div>
+          </div>
+          <div className={manual.manual__edit}>
+            <h2 className={manual.manual__sub__title}>
+              <span className={manual.underBar}>create(update)の見方</span>
+            </h2>
+            <div className={manual.manual__edit__list__box}>
+              <ol className={manual.manual__edit__list}>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Date
+                  </span>
+                  <span className={manual.manual__edit__text}>
+                    作成日時（西暦・月・日）
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Group Name
+                  </span>
+                  <span className={manual.manual__edit__text}>
+                    グループ名選択欄です。グループ作成はメニューの”Group”より作成してください。
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    User
+                  </span>
+                  <span className={manual.manual__edit__text}>
+                    デフォルトは、登録時のアカウントEmailになっています。お好みで変更してください。
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Coffee
+                  </span>
+                  <span className={manual.manual__edit__text}>
+                    コーヒー名や番号など
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Roast
+                  </span>
+                  <span className={manual.manual__edit__text}>
+                    焙煎度の種類で、数字が大きいほど焙煎度が高いです。（焙煎度が最も低い：Light
+                    roast（ライト ロースト）/焙煎度が最も高い：Italian
+                    roast（イタリアン）になります）
+                  </span>
+                  <ul className={manual.manual__edit__roast__list}>
+                    <li className={manual.manual__edit__roast__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        Light roast（ライト ロースト）
+                      </span>
+                    </li>
+                    <li className={manual.manual__edit__aroma__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        Cinnamon roast（シナモン）
+                      </span>
+                    </li>
+                    <li className={manual.manual__edit__aroma__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        Medium roast（ミディアム）
+                      </span>
+                    </li>
+                    <li className={manual.manual__edit__aroma__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        High roast（ハイ）
+                      </span>
+                    </li>
+                    <li className={manual.manual__edit__aroma__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        City roast（シティ）
+                      </span>
+                    </li>
+                    <li className={manual.manual__edit__aroma__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        Full city roast（フルシティ）
+                      </span>
+                    </li>
+                    <li className={manual.manual__edit__aroma__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        French roast（フレンチ）
+                      </span>
+                    </li>
+                    <li className={manual.manual__edit__aroma__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        Italian roast（イタリアン）
+                      </span>
+                    </li>
+                  </ul>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Aroma
+                  </span>
+                  <ul className={manual.manual__edit__aroma__list}>
+                    <li className={manual.manual__edit__aroma__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        Dry
+                      </span>
+                    </li>
+                    <li className={manual.manual__edit__aroma__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        Crust
+                      </span>
+                    </li>
+                    <li className={manual.manual__edit__aroma__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        Break
+                      </span>
+                    </li>
+                  </ul>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Defects
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Clean Cap
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Sweet
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Acidity
+                  </span>
+                  <ul className={manual.manual__edit__strength__list}>
+                    <li className={manual.manual__edit__strength__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        Acidity Strength
+                      </span>
+                    </li>
+                  </ul>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Mouthfeel
+                  </span>
+                  <ul className={manual.manual__edit__strength__list}>
+                    <li className={manual.manual__edit__strength__list}>
+                      <span
+                        className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                      >
+                        Body Strength
+                      </span>
+                    </li>
+                  </ul>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Flavor
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    After
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Balance
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Overall
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Sub Total
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    TOTAL (+36点)
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    Impression
+                  </span>
+                </li>
+                <li className={manual.manual__edit__item}>
+                  <span
+                    className={`${manual.manual__edit__number} ${manual.manual__edit__text}`}
+                  >
+                    memo
+                  </span>
+                </li>
+              </ol>
+            </div>
+            {/*   <div className={manual.manual__img}>
+                 <Image
+                  src="/images/Vector.png"
+                  width={366}
+                  height={4108}
+                  priority
+                  alt="createページのイメージ画像その1"
+                /> 
+                 <Image
+                  src="/images/create_img2.png"
+                  width={500}
+                  height={600}
+                  priority
+                  alt="createページのイメージ画像その2"
+                />
+                <Image
+                  src="/images/create_img3.png"
+                  width={500}
+                  height={600}
+                  priority
+                  alt="createページのイメージ画像その3"
+                />
+                <Image
+                  src="/images/create_img4.png"
+                  width={500}
+                  height={600}
+                  priority
+                  alt="createページのイメージ画像その4"
+                />
+                <Image
+                  src="/images/create_img5.png"
+                  width={500}
+                  height={600}
+                  priority
+                  alt="createページのイメージ画像その5"
+                />
+                <Image
+                  src="/images/create_img6.png"
+                  width={500}
+                  height={600}
+                  priority
+                  alt="createページのイメージ画像その6"
+                /> 
+              </div>*/}
           </div>
         </div>
       </div>
